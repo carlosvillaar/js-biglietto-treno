f2cf52d7741ca5bb64f8fef177d04360315b1039
carlosvillaar <carlosvillar004@gmail.com>
2024-11-13 14:10:31 +0100

    feat: add logic to script

---
+++ script.js
@@ -0,0 +1,23 @@
+let km = prompt(`inserisci il numero di chilometri che vuoi percorrere`);
+console.log(`Vuoi percorrere` + ` ` + km + ` ` + `chilometri`)
+
+let age = prompt(`inserisci la tua eta`);
+console.log(`Hai` + ` ` + age + ` ` + `anni` )
+
+const kmPrice = 0.21;
+
+let ticketPrice = (km * kmPrice)
+
+if (age < 18){
+  (ticketPrice / 100 * 20)
+  console.log(`Dato che hai` + ` ` + age + ` ` + `anni viene applicato uno sconto del 20%`)
+  ticketPrice.toFixed(2)
+}
+
+if (age > 65){
+  (ticketPrice / 100 * 40)
+  console.log(`Dato che hai` + ` ` + age + ` ` + `anni viene applicato uno sconto del 40%`)
+}
+
+console.log(`Il prezzo del biglietto e/' di` + ` ` + ticketPrice + ` ` + `euro`)
+
